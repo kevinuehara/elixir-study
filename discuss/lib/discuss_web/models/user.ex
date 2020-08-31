fa7bb5fd6c0939877cc758621c9e8e65514531fd
Kevin Uehara <kevin.uehara@wavy.global>
2020-08-30 23:38:01 -0300

    feat: implementing websockets in phoenix

--- discuss/lib/discuss_web/models/user.ex
+++ discuss/lib/discuss_web/models/user.ex
@@ -1,11 +1,14 @@
 defmodule DiscussWeb.User do
   use DiscussWeb, :model
 
+  @derive {Jason.Encoder, only: [:email]}
+
   schema "users" do
     field :email, :string
     field :provider, :string
     field :token, :string
     has_many :topics, DiscussWeb.Topic
+    has_many :comments, DiscussWeb.Comment
 
     timestamps()
   end
